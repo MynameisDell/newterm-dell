d2c0d0073447c2b43b90460de3cdd954fdf1da06
Dell <dvq273665536@gmail.com>
2023-06-07 18:14:06 +0700

    update

---
+++ js/commands.js
@@ -0,0 +1,242 @@
+const TWITTER = "https://twitter.com/Dell061297";
+const LINKEDIN = "https://www.linkedin.com/in/quangg-quangg-26ab45150/";
+const TELEGRAM = "https://t.me/MynameisDell/";
+const REPO = "https://dell-dusky.vercel.app/";
+const EMAIL = 'dvq273665536@gmail.com';
+const USERNAME = "Dell";
+
+// const ABOUT = [
+//   "<br>",
+//   "Hello and welcome to my portfolio website!",
+//   "My name is Eric, and I'm a final year Computer Science and Engineering student.",
+//   "Throughout my degree, I've honed my skills in algorithms, mathematics, and other",
+//   "technical fields. I'm passionate about technology and enjoy learning new things, ",
+//   "as well as tackling challenges with creativity and innovation. In my free time,", 
+//   "I enjoy reading, exercising, and playing video games occasionally. I believe that ",
+//   "a balanced lifestyle is essential to maintaining a healthy body and mind, which is ",
+//   "crucial for success in any field. As I approach the end of my studies, I'm excited ",
+//   "to explore job opportunities and research collaborations. I'm passionate about being",
+//   "at the forefront of technological innovation and making a positive impact in the world.",
+//   "<br>"
+// ];
+
+const ABOUT = [
+  "<br>",
+  "Hello I am Dell!👋" ,
+  "              ",
+  "I'm an ordinary Programmer & on this Earth. ",
+  "Specifically, I am an Asian, ",
+  "more simply I was born in a country in Southeast",
+  "Asia and especially I am Vietnamese. 🧑‍💻",
+  "                            ",
+  "                            ",
+  "My hobby is traveling all over the country,", 
+  "that's why I decided to learn to code..",
+  "hone my skills in algorithms, math",
+  "                             ",
+  "                             ",
+  "You can find me through social networks: 💫",
+  "<br>"
+];
+
+
+SOCIAL = [
+  "<br>",
+  'twitter        <a href="' + TWITTER + '" target="_blank">twitter click/' + USERNAME + '</a>',
+  'linkedin       <a href="' + LINKEDIN + '" target="_blank">linkedin click/' + USERNAME + "</a>",
+  'telegram       <a href="' + TELEGRAM + '" target="_blank">telegram click/' + USERNAME + '</a>',
+  'repo           <a href="' + REPO + '" target="_blank">repo click/' + USERNAME + "</a>",
+  "<br>"
+];
+
+
+PROJECTS = [
+  "<br>",
+  "<strong>Check out some of my other Channel",
+];
+
+COMMAND_LIST = [
+  "<br>",
+  '<span class="command">about</span>          Learn more about me',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">channel</span>        Show my Channel',
+  '<span class="command">date</span>           Display the actual date',
+  '<span class="command">projects</span>       View some of my projects',
+  '<span class="command">history</span>        Show command history',
+  '<span class="command">email</span>          Feel free to reach out',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">clear | cls</span>    Clear terminal',
+  '<span class="command">echo</span>           Print any give string',
+  '<span class="command">ls</span>             List directory content',
+  '<span class="command">cd &lt;directory&gt</span> Change directory',
+  '<span class="command">help</span>           Display all commands',
+  "<br>",
+];
+
+const BANNER = [
+"  _______   ______    ______   _______  __   __  _______  _______ ",
+" |  ____ | |  __  |  |  ____| |  _____| \\ \\ / / |  ____ ||  ____|",
+" | |    || | |  | |  | |__    | |__      \\ V /  | |__| || |__   ",
+" | |    || | |  | |  |  __|   |  __|      > <   |  __  ||  __|  ",
+" | |____|| | |__| |  | |____  | |____    / . \\  | |  | || |____ ",
+" |______|| |______|  |______| |______|  /_/ \\_\\ |_|  |_||______|",
+"        _______  __   __  _______  _______  _______ ",
+"       |  ____ ||  \\ /  | |  ____ ||  ____ ||  ____ |",
+"       | |__| || \\ V / | | |__| || |__| || |__    ",
+"       |  __  ||  > <  | |  __  ||  __  ||  __|   ",
+"       | |  | || / . \\ | | |  | || |  | || |____ ",
+"       |_|  |_||_/ \\_\\| |_|  |_||_|  |_||______|",
+];
+
+DIRECTORIES = [ 
+"<br>",
+"photos",
+"music",
+"videos",
+"<br>"
+]
+
+
+
+EMAIL_INFO = [
+  "                 ",
+  "✉️ " + "<a href=mailto:" + EMAIL + ">" + EMAIL +"</a>",
+  "                  "
+]
+
+SUDO = [
+"                                                                ",
+"Only Eric has <span class=\"command\">admin</span> privileges :(",
+"                                                                "
+]
+
+CHANNEL_INFO = [
+  "<br><strong>Studied at Ba Ria - Vung Tau University</strong>",
+  "Ranked good in Accounting Faculty, majoring in Accounting and Auditing",
+  "                                                  ",
+  "<strong>Used to work at Lotte Mart Corporation, the owner of Korea, to pay quickly in Vietnam</strong>",
+  "Working as a programmer for a foreign company.",
+  "                                               ",
+  "<strong>Currently doing many jobs to earn a living, in which going to other people is a side job.</strong>",
+  "<br>"
+]
+
+MOBILE_CHANNEL_INFO = [
+  "<br><strong>Studied at Ba Ria - Vung Tau University</strong>",
+  "Ranked good in Accounting Faculty", 
+  "majoring in Accounting and Auditing",
+  "<br>",
+  "<strong>Used to work at Lotte Mart Corporation",
+  "the owner of Korea",
+  "to pay quickly in Vietnam</strong>",
+  "Become a programmer",
+  "and collaborators for foreign companies",
+  "<br>",
+  "<strong>Current job.",
+  "Clinging to the family</strong>",
+  "<br>"
+]
+
+
+TERMINAL_INFO = [
+'Welcome to my interactive web terminal profile',
+'As a passionate user and love the Linux operating system.',
+'Especially the terminal.',
+'So I came up with the idea to develop my personal website terminal style.',
+"For a list of available commands, type <span class=\"command\">'help'</span>.",
+"                                                                             ",
+"Version (4.0)",
+
+]
+
+TERMINAL_INFO_MOBILE = [
+  'Welcome to my terminal web profile!',
+  'A user who loves Linux.',
+  'Especially the terminal.',
+  'So I came up with a development idea',
+  'my personal website',
+  'terminal style.',
+  "                                 ",
+  "For a list of available commands,", 
+  "type <span class=\"command\">'help'</span>.",
+  "                                          ",
+  "Version (4.0)",
+  "                                          "
+  ]
+
+
+
+// -----------------------SAMPLE ASCII ARTS --------------------
+
+// "   / ____/____(_)____   /   |  / /___  (_)___  ____ ______  ",
+// "  / __/ / ___/ / ___/  / /| | / / __ \\/ /_  / / __ `/ ___/ ",
+// " / /___/ /  / / /__   / ___ |/ / /_/ / / / /_/ /_/ / /      ",
+// "/_____/_/  /_/\\___/  /_/  |_/_/ .___/_/ /___/\\__,_/_/     ",
+// "                             /_/                            "
+
+const MOBILE_BANNER = [
+"    _    _      _ _                             ",
+"   / \\  (_)_ __| | | ___   __ _  ___  ___  _ __ ",
+"  / _ \\ | | '__| | |/ _ \\ / _` |/ _ \\/ _ \\| '__|",
+" / ___ \\| | |  | | | (_) | (_| |  __/ (_) | |   ",
+"/_/   \\_\\_|_|  |_|_|\\___/ \\__, |\\___|\\___/|_|   ",
+"                          |___/                 ",
+"      ____                                       ",
+"     /\\  _`\\       __                           ",
+"     \\ \\ \\L\\ \\     /\\ \\__                        ",
+"      \\ \\ ,  /  __ \\ \\ ,_\\   _ __    __         ",
+"       \\ \\ \\/  /\\`'\\`\\ \\ \\/ /\\`'__\\/'__`\\       ",
+"        \\ \\ \\_/\\ \\ \\ \\ \\ \\_\\ \\ \\//\\ \\L\\.\\_     ",
+"         \\ `\\___\\''\\ \\_\\ \\____/\\ \\_\\ \\__/.\\_\\    ",
+"          `\\/__/   \\/_/\\/___/  \\/_/\\/__/\\/_/    ",
+];
+
+
+
+// "   ____    _       ___   __     _               ",
+// "  / __/___(_)___  / _ | / /__  (_)__ ___ _____  ",
+// " / _// __/ / __/ / __ |/ / _ \\/ /_ // _ `/ __/ ",
+// "/___/_/ /_/\\__/ /_/ |_/_/ .__/_//__/\\_,_/_/   ",
+// "                       /_/                      ",
+
+
+// "                                                  |",
+// "                                                \\ _ /",
+// "                                              -= (_) =-",
+// "                                                /   \\              _\\/_",
+// "   ____    _       ___   __     _                 |                //o\\  _\\/_",
+// "  / __/___(_)___  / _ | / /__  (_)__ ___ _____  _____ _ __ __ ____ _ | __/o\\\\ _",
+// ' / _// __/ / __/ / __ |/ / _ \\/ /_ // _ `/ __/ =-=-_-__=_-= _=_=-=_,-' + `|"'""-|-,_`,
+// `/___/_/ /_/\\__/ /_/ |_/_/ .__/_//__/\\_,_/_/   =- _=-=- -_=-=_,-"           |`,
+// "                      /_/                      === =- =- -=.--",
+
+
+
+
+// "        .n.                     |",
+// "       /___\\          _.---.  \\ _ /",
+// "       [|||]         (_._ ) )--;_) =-",
+// "       [___]           '---'.__,' \\ ",
+// `       }-=-{                    |`,
+// '       |-" |',
+// '       |.-"|                p',
+// "~^=~^~-|_.-|~^-~^~ ~^~ -^~^~|\\ ~^-~^~-",
+// `^   .=.| _.|__  ^       ~  /| \\ `,
+// ' ~ /:. \\" _|_/\\    ~      /_|__\\  ^',
+// '.-/::.  |   |""|-._    ^   ~~~~',
+// "  `===-'-----' "+ `""` + "`  '-.              ~",
+// "                 __.-'      ^",
+
+
+
+
+// "|",
+// "\ _ /",
+// "-= (_) =-",
+// "/   \         _\/_",
+// "  |           //o\  _\/_",
+// "_____ _ __ __ ____ _ | __/o\\ _",
+// `=-=-_-__=_-= _=_=-=_,-'|"'""-|-,_`,
+// `=- _=-=- -_=-=_,-"          |`,
+// "jgs =- =- -=.--",
+
