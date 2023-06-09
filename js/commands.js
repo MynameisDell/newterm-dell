b81bf604f040ca4ee236e5c35207c3c1b92c7396
Dell <dvq273665536@gmail.com>
2023-06-09 12:44:08 +0700

    update

--- js/commands.js
+++ js/commands.js
@@ -38,7 +38,7 @@ const ABOUT = [
   "You can find me through social networks: 💫",
   "                                            ",
   "𝗟𝗮𝗻𝗴𝘂𝗮𝗴𝗲"
-  "<img src='href="img/man.png"' alt='Your Image'>",
+  "<img src='img/lan.png' alt='Your Image'>",
   "<br>"
 ];
 
